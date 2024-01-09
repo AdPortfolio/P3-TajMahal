d345be2ae89445914fcf43d8cc04d7ffc3fc42fb
lab@walimaloui.com <lab@walimaloui.com>
2024-01-09 18:31:24 +0100

    Construct DetailsView

---
+++ TajMahal/TajMahal/Views/Details Views/DetailsView.swift
@@ -0,0 +1,33 @@
+//
+//  DetailsView.swift
+//  TajMahal
+//
+//  Created by Walim Aloui on 09/01/2024.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    
+    var meal: Dish?
+    
+    var body: some View {
+        NavigationStack {
+            if let meal = meal {
+                DetailsImageView(meal: meal)
+                    .padding(.top, 10)
+                DetailsDescriptionView(meal: meal)
+            }
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: MenuBackButton(textTitle: meal?.name ?? "Back"))
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailsView()
+            .environmentObject(ViewModel())
+    }
+}
