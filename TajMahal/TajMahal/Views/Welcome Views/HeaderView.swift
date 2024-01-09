0efec0c758100baa8b7981d5be54ee5ef1d4d98e
lab@walimaloui.com <lab@walimaloui.com>
2024-01-09 16:40:50 +0100

    Add RestaurantInfoView

--- TajMahal/TajMahal/Views/Welcome Views/HeaderView.swift
+++ TajMahal/TajMahal/Views/Welcome Views/HeaderView.swift
@@ -5,14 +5,43 @@
 //  Created by Walim Aloui on 09/01/2024.
 //
 
+
 import SwiftUI
 
 struct HeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Image("TajMahal")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding(.leading, 10)
+            .padding(.trailing, 10)
+            .padding(.bottom)
+        
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Restaurant Indien")
+                    .font(.caption)
+                    .fontWeight(.light)
+                
+                    .foregroundStyle(.gray)
+                Text("Taj Mahal")
+                    .customPlusJakartaSansSemiBold(size: 20)
+                    .fontWeight(.bold)
+            }
+            .padding(.leading, 10)
+            
+            Spacer()
+            
+            Image("Logo")
+                .resizable()
+                .foregroundColor(Color.gray)
+                .frame(width: 40, height: 40)
+                .padding(.trailing, 10)
+        }
     }
 }
 
-#Preview {
+#Preview(traits: .sizeThatFitsLayout) {
     HeaderView()
 }
