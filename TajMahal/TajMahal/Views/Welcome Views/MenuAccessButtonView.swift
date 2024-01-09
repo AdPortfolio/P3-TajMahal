9817f205a0e174c4c4d5e427fc68d38f8af5daa3
lab@walimaloui.com <lab@walimaloui.com>
2024-01-09 16:47:36 +0100

    Add MenuAccessButtonView

---
+++ TajMahal/TajMahal/Views/Welcome Views/MenuAccessButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuAccessButtonView.swift
+//  TajMahal
+//
+//  Created by Walim Aloui on 09/01/2024.
+//
+
+import SwiftUI
+
+struct MenuAccessButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuAccessButtonView()
+}
