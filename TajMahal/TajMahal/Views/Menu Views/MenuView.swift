c0993dfc5c28db0fa17fdcbf382f71c524c0b0bb
lab@walimaloui.com <lab@walimaloui.com>
2024-01-09 18:32:12 +0100

    Construct MenuView

--- TajMahal/TajMahal/Views/Menu Views/MenuView.swift
+++ TajMahal/TajMahal/Views/Menu Views/MenuView.swift
@@ -7,18 +7,66 @@
 
 import SwiftUI
 
-// Menu sous forme de liste
 struct MenuView: View {
-    // Référence vers le view model qui permet d'accéder aux tableaux d'entrées et de plats du menu
-    let viewModel: ViewModel = ViewModel()
+    
+    @EnvironmentObject var viewModel: ViewModel
+    
+    init() {
+        UINavigationBar.appearance().titleTextAttributes = [.font : UIFont(name: "PlusJakartaSans-Bold", size: 20)!, .foregroundColor : UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1)]
+    }
     
     var body: some View {
-        List {
-            // À completer
+        NavigationStack {
+            List {
+                Section {
+                    Text("Entrées")
+                        .customPlusJakartaSansSemiBold(size: 16)
+                        .customGray()
+                    
+                    ForEach(viewModel.apetizerArray, id: \.self) { dish in
+                        
+                        MenuMealRowView(imageName: dish.imageName,
+                                        title: dish.name,
+                                        description: dish.description,
+                                        price: dish.price!,
+                                        spiceLevel: dish.spiceLevel, meal: dish)
+                    }
+                }
+                .listRowBackground(Color.clear)
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
+                
+                
+                Section {
+                    Text("Plats Principaux")
+                        .customPlusJakartaSansSemiBold(size: 16)
+                        .customGray()
+                    
+                    ForEach(viewModel.mainCourseArray, id: \.self) { meal in
+                        MenuMealRowView(
+                            imageName: meal.imageName,
+                            title: meal.name,
+                            description: meal.description,
+                            price: meal.price ?? "€",
+                            spiceLevel: meal.spiceLevel, meal: meal)
+                    }
+                    
+                }
+                .listRowBackground(Color.clear)
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
+            }
         }
+        .navigationBarTitle(Text("Menu"))
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: MenuBackButton(textTitle: ""))
+        
     }
 }
 
 #Preview {
-    MenuView()
+    NavigationStack {
+        MenuView()
+    }
+    .environmentObject(ViewModel())
 }
