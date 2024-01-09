0c197063f5a3f70dc32c06c59fe45caab0496132
lab@walimaloui.com <lab@walimaloui.com>
2024-01-09 18:30:52 +0100

    Add DetailsImageView component

---
+++ TajMahal/TajMahal/Views/Details Views/DetailsImageView.swift
@@ -0,0 +1,26 @@
+//
+//  DetailsImageView.swift
+//  TajMahal
+//
+//  Created by Walim Aloui on 09/01/2024.
+//
+
+import SwiftUI
+
+struct DetailsImageView: View {
+    
+    var meal: Dish
+    var body: some View {
+        
+        Image(meal.imageName)
+            .resizable()
+            .scaledToFill()
+            .frame(maxWidth: 335, maxHeight: 467)
+            .padding(.leading, 15)
+            .padding(.trailing, 15)
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    DetailsImageView(meal: Dish(name: "name", description: "Description", allergens: "Allgergens", ingredients: "ingredients", spiceLevel: .hot, imageName: "Pakoras", price: "XXX"))
+}
