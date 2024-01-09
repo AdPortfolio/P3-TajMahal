23d4df554d0383c21d198ef4412b4d5d2498d9b7
lab@walimaloui.com <lab@walimaloui.com>
2024-01-09 16:28:21 +0100

    Create folders for Model and ViewModel files

--- TajMahal/TajMahal/Models/Model.swift
+++ TajMahal/TajMahal/Models/Model.swift
@@ -7,14 +7,12 @@
 
 import Foundation
 
-// Cette enumération répertorie 3 niveaux de piment
 enum SpiceLevel {
     case light
     case medium
     case hot
 }
 
-// Représente l'objet "plat", qui figure sur la carte du menu
 struct Dish {
     var name: String
     var description: String
