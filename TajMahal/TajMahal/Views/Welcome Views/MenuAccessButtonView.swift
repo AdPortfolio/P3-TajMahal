877793bf034f178d382a5c2a98f2b4559052ff32
lab@walimaloui.com <lab@walimaloui.com>
2024-01-09 16:49:46 +0100

    Add MenuAccessButtonView

--- TajMahal/TajMahal/Views/Welcome Views/MenuAccessButtonView.swift
+++ TajMahal/TajMahal/Views/Welcome Views/MenuAccessButtonView.swift
@@ -9,10 +9,21 @@ import SwiftUI
 
 struct MenuAccessButtonView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink(destination: MenuView()) {
+            Text("Accéder au menu")
+                .fontWeight(.bold)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .foregroundStyle(Color.white)
+                .padding(10)
+                .background(Color("CustomRed"))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+        .padding(.leading, 10)
+        .padding(.trailing, 10)
+        .padding(.bottom, 5)
     }
 }
 
-#Preview {
+#Preview(traits: .sizeThatFitsLayout) {
     MenuAccessButtonView()
 }
